c95fcac51cae224350a86896e7ad882a938137b8
email <mark.akula@student.tdsb.on.ca>
2022-11-17 13:51:09 -0500

    Firebase files

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCz3tinGy3KHvMlN7IdQkaIXIK-3nLmybE",
+  authDomain: "ics4u-6a7c7.firebaseapp.com",
+  projectId: "ics4u-6a7c7",
+  storageBucket: "ics4u-6a7c7.appspot.com",
+  messagingSenderId: "814800206696",
+  appId: "1:814800206696:web:dbd674ade6e7807cb134dd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
